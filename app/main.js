d60b5a567b924682adcffa477a4a15308390df9f
Nitish Varma <107538573+cb-nitish-varma@users.noreply.github.com>
2024-03-31 01:04:43 +0530

    handle parsing all headers

--- app/main.js
+++ app/main.js
@@ -6,15 +6,38 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const requestData = data.toString().split("\r\n");
-    const httpStartLine = requestData[0].split(" ");
+    const httpObj = {
+      method: "",
+      path: "",
+      headers: {},
+    };
 
-    const parsedPath = httpStartLine[1].replace("/echo/", "");
+    const requestData = data.toString().split("\r\n\r\n")[0].split("\r\n");
+    const httpStartLine = requestData[0].split(" ");
+    httpObj.method = httpStartLine[0];
+    httpObj.path = httpStartLine[1];
+
+    const requestHeaders = requestData.slice(1);
+
+    requestHeaders.forEach((headerLine) => {
+      const splitHeader = headerLine.split(": ");
+      httpObj.headers[String(splitHeader[0])] = String(splitHeader[1]);
+    });
+
+    const parsedPath = httpObj.path.replace("/echo/", "");
     const parsedPathLength = parsedPath.length;
 
-    if (httpStartLine[1] == "/" || httpStartLine[1].startsWith("/echo")) {
+    if (httpObj.path == "/") {
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${httpObj.headers["User-Agent"].length}\r\n\r\n${httpObj.headers["User-Agent"]}\r\n\r\n`
+      );
+    } else if (httpObj.path.startsWith("/echo")) {
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${parsedPathLength}\r\n\r\n${parsedPath}\r\n\r\n`
+      );
+    } else if (httpObj.path.startsWith("/user-agent")) {
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${httpObj.headers["User-Agent"].length}\r\n\r\n${httpObj.headers["User-Agent"]}\r\n\r\n`
       );
     } else {
       socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
