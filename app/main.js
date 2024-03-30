9b4fbfa22498b5dc20648b9865b0dfa4ea2730fc
Nitish Varma <107538573+cb-nitish-varma@users.noreply.github.com>
2024-03-30 22:46:17 +0530

    send a 200 ok

--- app/main.js
+++ app/main.js
@@ -5,10 +5,14 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+
   socket.on("close", () => {
     socket.end();
     server.close();
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, "localhost", () => {
+  console.log("server bound port 4221");
+});
