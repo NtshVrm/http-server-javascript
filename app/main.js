961d15d361af1c0f61b5e0b37e2b5b4e332bb160
Nitish Varma <107538573+cb-nitish-varma@users.noreply.github.com>
2024-03-30 23:01:16 +0530

    parse request path to respond with 404

--- app/main.js
+++ app/main.js
@@ -5,8 +5,16 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    const requestData = data.toString().split("\r\n");
+    const httpStartLine = requestData[0].split(" ");
 
+    if (httpStartLine[1] == "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+  });
   socket.on("close", () => {
     socket.end();
     server.close();
